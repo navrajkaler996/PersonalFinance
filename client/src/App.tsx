e93522db6247ca2f2dce10d3762ffdf66b5a4c61
Navraj singh kaler <navrajkaler996@gmail.com>
2024-03-07 18:58:23 -0600

    project setup finished and TailwindCSS installed

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="app">
+        <h1 className="text-yellow-500">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
